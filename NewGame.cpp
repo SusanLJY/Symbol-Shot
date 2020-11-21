377e8e8917fcd0990c8607f6214e454ca937b4c4
SusanLJY <70014546+SusanLJY@users.noreply.github.com>
2020-11-21 17:38:03 +0800

    Update NewGame.cpp

--- NewGame.cpp
+++ NewGame.cpp
@@ -1,3 +1,19 @@
+/* NewGame.cpp 
+ This file aims at doing preparation jobs for users to start a new game.
+  Variables and Functions needed:
+    1. external variables: 
+      1.1 vector<string> userName: store userName in a vector //dynamic memory
+      1.2 int level: initialize the level of games
+      1.3 bool newBoard: determine whether we need to regenerate a new board or using the previous one.
+    2. void restoringUserNameRecord():
+      Restore the user name from UserNameRecord.txt file. 
+      Exception dealing: 
+        If the file does not exist, then create an empty file.
+    3. void newGame():
+      Get current GMT and input a new user name (without ' '). Store the user name in both vector and txt file. Then move to play stage.
+      Exception dealing: 
+        If the user name is used before, it will ask you to input another.
+*/
 #include <iostream>
 #include <fstream>
 #include <time.h>
@@ -10,29 +26,14 @@
 #include "Timer.h"
 #include "Playing.h"
 using namespace std;
-/* Functions needed:
-    1. string currentTime();
-        Get current time (Format DD/MM/YYYY HH:MM HKT) (import)
-        return current_time
 
-    2. array[][3] storeLevel(string level, string user_name, array[][3] record_level):
-        Store the level, user's name of last players, and the current time. --> (record_level[10][3]={[0,0,0]})
-        return record_level;
-
-    3. string newUserName():
-        cin>>user_name
-        Get new user's name (while loop)
-            (if overlapped with previous players, cout<< "The name is used. Please change to another name.", recall this function)
-        return user_name
-    4. Call playing 
-*/
 vector<string> userName;
 int level;
 bool newBoard;
 
 void restoringUserNameRecord(){
   ifstream fin("UserNameRecord.txt", ifstream::out);
-	if ( fin.fail() ){
+	if ( fin.fail() ){//dealing with exception
 		ofstream fout("UserNameRecord.txt");
 		restoringUserNameRecord();
  	}
@@ -62,7 +63,7 @@ void newGame(){
   userName.push_back(un);
 
   ofstream fout("UserNameRecord.txt", ios::app);
-  if (fout.fail()) {
+  if (fout.fail()) { //dealing with exception
 		cout << "Error in saving user-name information!" << endl;
     menu();
   }
