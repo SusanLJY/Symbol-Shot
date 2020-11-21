29bd02c8060512f5f6ae1035fefec0e1febe30a2
SusanLJY <70014546+SusanLJY@users.noreply.github.com>
2020-11-21 17:51:00 +0800

    Update Timer.cpp

--- Timer.cpp
+++ Timer.cpp
@@ -1,33 +1,12 @@
-// Timer.cpp
+/* Timer.cpp
+  This file is to store the current timing status and return the time of every points. It also has the functions to pause and stop timing.
+*/
 #include <iostream>  
 #include <cstdlib>  
 #include <ctime>    
 #include <iomanip> 
 #include "NewGame.h"
 using namespace std;
-
-// class Timer{  
-//     private:   
-//         long pause_time;  
-//         //Using 2 boolean to symbolize 4 status   
-//         bool is_pause = false; //Pause or not  
-//         bool is_stop = true;//Stop or not 
-//     public:
-//         long start_time; 
-//         bool isPause(); //Return the status of Timer
-//         bool isStop();  
-//         //3 function of Timer
-//         void Start();   
-//         void Pause();  
-//         void Stop();  
-//         inline long getStartTime() {return start_time;}
-//         void currentTime(char *);
-// }; 
-
-// Timer::Timer(){  
-//     is_pause = false; //Initialize Timer. 
-//     is_stop = true;  
-// } 
 
 bool Timer::isPause(){     
     if(is_pause)  return true;  
@@ -47,20 +26,17 @@ void Timer::Start(){
     }else start_time = time(0);
 } 
 void Timer::Pause(){  
-     //如果处于停止/暂停状态,此动作不做任何处理，直接返回
     if(is_stop||is_pause) return;   
-    //否则调制为暂停状态  
     else{  
         is_pause = true;  
-        pause_time = time(0); //获取暂停时间
+        pause_time = time(0); //Get the length of  pausing time
         cout<<"\nGame Paused!\nInput \"C\" to continue!"<<endl;   
     }  
 }
 
 void Timer::Stop(){  
-    //如果正处于停止状态（不是暂停状态），不做任何处理
     if(is_stop) return ;   
-    else if(is_pause){ //改变计时器状态   
+    else if(is_pause){ //Change the status of timer  
         is_pause = false;  
         is_stop = true;  
     }  
