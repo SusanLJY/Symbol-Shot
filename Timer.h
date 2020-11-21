c0ad219a0e200680bf2b59012b4a54be75fb918d
SusanLJY <70014546+SusanLJY@users.noreply.github.com>
2020-11-21 18:06:58 +0800

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -11,21 +11,21 @@
 using namespace std;
 //Declare the Timer class
 class Timer{  
-    private:   
-        long pause_time;  
-        //Using 2 boolean to symbolize 4 status   
-        bool is_pause; //Pause or not  
-        bool is_stop;//Stop or not 
-    public:
-        long start_time;  
-        bool isPause();
-        bool isStop();  
-        //functions of Timer
-        void Start();   
-        void Pause();  
-        void Stop();  
-        inline long getStartTime() {return start_time;} 
-        void currentTime(int *);
+  private:   
+    long pause_time;  
+    //Using 2 boolean to symbolize 4 status   
+    bool is_pause; //Pause or not  
+    bool is_stop;//Stop or not 
+  public:
+    long start_time;  
+    bool isPause();
+    bool isStop();  
+    //functions of Timer
+    void Start();   
+    void Pause();  
+    void Stop();  
+    inline long getStartTime() {return start_time;} 
+    void currentTime(int *);
 };
  
 #endif
