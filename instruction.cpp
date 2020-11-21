3178788361845dd5e2f44bd7ae87a9b1b1fd1871
SusanLJY <70014546+SusanLJY@users.noreply.github.com>
2020-11-21 14:58:08 +0800

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -1,3 +1,9 @@
+/* Workflow
+  1. Read the instruction.txt and deal with exceptions.
+  2. If read successfully --> print the instructions of playing Symbol-Shot.
+  3. Press 'M' to return to menu.
+    Exception dealing: continuously get input until input 'M'.
+*/
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -7,7 +13,7 @@ using namespace std;
 void instruction() {
   ifstream fin;
   fin.open("instruction.txt");
-  if (fin.fail()){
+  if (fin.fail()){ //deal with exceptions
     cout << "I'm sure you are so clever that you can play the game without instructions!" << endl;
     return;
   }
@@ -20,7 +26,7 @@ void instruction() {
 	cout << "  \n";
   char co;
   cin>>co;
-  while (co!='M'){
+  while (co!='M'){ //deal with exceptions
     cout<<"Input again!"<<endl;
     cin>>co;
   }
